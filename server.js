b524ce0eb2798f911b736d1550312181930aa779
shimizu <xxshimizuxx@gmail.com>
2013-10-15 11:54:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,26 +10,17 @@ http.createServer(function(request, response) {
 	var Response = {
 		200:function(file, filename){
 			var extname = path.extname(filename);
-			var headerBase = {
+			var header = {
 				'Access-Control-Allow-Origin':'*',
 				'Pragma': 'no-cache',
 				'Cache-Control' : 'no-cache'	   
 			}
 
-			var headerStr = {};
-			headerStr['.csv'] = headerStr['.json'] = headerBase;
-			headerStr['.json']['Content-Type'] = 'application/json; charset=utf-8';		
-			headerStr['.csv']['Content-Type'] = 'text/csv;  charset=utf-8';
-
-
-			headerStr['.topojson'] = headerStr['.geojson'] =  headerStr['.json'];
-			var header = (headerStr[extname]) ? headerStr[extname] : null;
-
 			response.writeHead(200, header);
 			response.write(file, "binary");
 			response.end();
 		},
-		404:function(){
+		"404":function(){
 			response.writeHead(404, {"Content-Type": "text/plain"});
 			response.write("404 Not Found\n");
 			response.end();
@@ -48,7 +39,8 @@ http.createServer(function(request, response) {
 	, filename = path.join(process.cwd(), uri);
 
 	fs.exists(filename, function(exists){
-		if (!exists) { Response[404]; return ; }
+		console.log(filename+" "+exists);
+		if (!exists) { Response['404'](); return ; }
 		if (fs.statSync(filename).isDirectory()) { filename += '/index.html'; }
 
 		fs.readFile(filename, "binary", function(err, file){
